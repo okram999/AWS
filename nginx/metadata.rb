1287c9216933a3a4567b81d7557416171c484b1c
robot123 <okram@robots.com>
2015-05-21 15:01:06 -0700

    second hub

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -1,7 +1,7 @@
 name             'nginx'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'pornhub.com'
+maintainer_email 'admin@pornhub.com'
 license          'All rights reserved'
 description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
